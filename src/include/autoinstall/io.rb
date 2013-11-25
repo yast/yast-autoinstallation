b06768c9dc2684516f2bc9be832757db1d18ffbf
Thomas Fehr <fehr@suse.de>
2013-11-25 17:27:49 +0100

    fix bug handling url schemes device: and usb: (bnc#849767)

--- src/include/autoinstall/io.rb
+++ src/include/autoinstall/io.rb
@@ -452,8 +452,13 @@ module Yast
           end
           Builtins.foreach(deviceList) do |_Host2|
             Builtins.y2milestone("looking for profile on %1", _Host2)
+            # this is workaround for bnc#849767
+            # because of changes in autoyast startup this code is now
+            # called much sooner (before Storage stuff is initialized)
+            # call dummy method to trigger Storage initialization
+            dummy = Storage.GetUsedFs()
             mp = Storage.DeviceMounted(Ops.add("/dev/", _Host2))
-            already_mounted = !Builtins.isempty(mount_point)
+            already_mounted = !Builtins.isempty(mp)
             mount_point = mp if already_mounted
             Builtins.y2milestone(
               "already mounted=%1 mountpoint=%2 mp=%3",
