9619e06fc3dfb781e889764825c4b084e23584b3
Imobach González Sosa <imobachgs@gmail.com>
2015-04-21 13:40:30 +0100

    Disable COVERAGE because of troubles when requiring modules

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,17 +5,7 @@ require "yast"
 require "fileutils"
 
 if ENV["COVERAGE"]
-  require "simplecov"
-  SimpleCov.start
-  # for coverage we need to load all ruby files
-  Dir["#{root_location}/library/*/src/{module,lib}/**/*.rb"].each { |f| require_relative f }
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  end
+  STDERR.puts "COVERAGE is disabled because when requiring some modules (like AutoinstPartition) "\
+    "errors are raised in other YaST components."
 end
 
