1f47e097a0da8b2addd0f5de2b5f1c569038b477
Stefan Schubert <schubi@suse.de>
2015-05-12 10:47:31 +0200

    fixed testcases

--- test/Y2ModuleConfig_test.rb
+++ test/Y2ModuleConfig_test.rb
@@ -11,6 +11,27 @@ describe Yast::Y2ModuleConfig do
   FIXTURES_PATH = File.join(File.dirname(__FILE__), 'fixtures')
   DESKTOP_DATA = YAML::load_file(File.join(FIXTURES_PATH, 'desktop_files', 'desktops.yml'))
 
+  before do
+    allow(Yast::WFM).to receive(:ClientExists).with("audit-laf_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("autofs_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("ca_mgm_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("deploy_image_auto").and_return(true)
+    allow(Yast::WFM).to receive(:ClientExists).with("files_auto").and_return(true)
+    allow(Yast::WFM).to receive(:ClientExists).with("firstboot_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("general_auto").and_return(true)
+    allow(Yast::WFM).to receive(:ClientExists).with("language_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("pxe_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("restore_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("runlevel_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("scripts_auto").and_return(true)
+    allow(Yast::WFM).to receive(:ClientExists).with("software_auto").and_return(true)
+    allow(Yast::WFM).to receive(:ClientExists).with("sshd_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("sysconfig_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("unknown_profile_item_1_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("unknown_profile_item_2_auto").and_return(false)
+    allow(Yast::WFM).to receive(:ClientExists).with("partitioning_auto").and_return(false)
+  end
+
   describe "#unhandled_profile_sections" do
     let(:profile_unhandled) { File.join(FIXTURES_PATH, 'profiles', 'unhandled_and_obsolete.xml') }
 
@@ -22,7 +43,7 @@ describe Yast::Y2ModuleConfig do
         [
           "audit-laf", "autofs", "ca_mgm", "firstboot", "language", "restore",
           "runlevel", "sshd", "sysconfig", "unknown_profile_item_1",
-          "unknown_profile_item_2", "users"
+          "unknown_profile_item_2"
         ].sort
       )
     end
