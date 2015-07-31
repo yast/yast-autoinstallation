0bdc3ca0462df04508b1305a57b7e86b426a63a6
Stefan Schubert <schubi@suse.de>
2015-07-31 13:55:03 +0200

    install packages only if the have not already been installed

---
+++ test/AutoinstSoftware_test.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env rspec
+
+require_relative "test_helper"
+
+Yast.import "AutoinstSoftware"
+Yast.import "Profile"
+
+describe Yast::AutoinstSoftware do
+  subject { Yast::AutoinstSoftware }
+  FIXTURES_PATH = File.join(File.dirname(__FILE__), 'fixtures')
+  let(:profile) { File.join(FIXTURES_PATH, 'profiles', 'software.xml') }
+
+  before(:each) do
+    Yast::Profile.ReadXML(profile)
+  end
+
+  describe "post-software installation" do
+
+    it "installs packages only if the have not already been installed" do
+      Yast::AutoinstData.post_packages = ["a2"]
+      expect(Yast::PackageSystem).to receive(:Installed).with("a1").and_return(false)
+      expect(Yast::PackageSystem).to receive(:Installed).with("a2").and_return(false)
+      expect(Yast::PackageSystem).to receive(:Installed).with("a3").and_return(true)
+      subject.Import(Yast::Profile.current["software"])
+
+      expect(Yast::AutoinstData.send(:post_packages)).to eq(["a1","a2"])
+    end
+
+  end
+
+end
