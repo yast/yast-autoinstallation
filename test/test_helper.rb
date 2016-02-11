1e3910db39d97dc71a811f2bf3062608afdce584
Martin Vidner <mvidner@suse.cz>
2016-02-11 17:35:17 +0100

    Revert "Disable COVERAGE because of troubles when requiring modules"
    This reverts commit 9619e06fc3dfb781e889764825c4b084e23584b3.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,17 @@ require "yast/rspec"
 require "fileutils"
 
 if ENV["COVERAGE"]
-  STDERR.puts "COVERAGE is disabled because when requiring some modules (like AutoinstPartition) "\
-    "errors are raised in other YaST components."
+  require "simplecov"
+  SimpleCov.start
+  # for coverage we need to load all ruby files
+  Dir["#{root_location}/library/*/src/{module,lib}/**/*.rb"].each { |f| require_relative f }
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
 end
 
