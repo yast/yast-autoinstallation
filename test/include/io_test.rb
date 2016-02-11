5a673cfe7c83fd24ff58fe83acc2e941438b1425
Martin Vidner <mvidner@suse.cz>
2016-02-11 15:35:47 +0100

    Test AutoinstallIoInclude#Get: done covering usb+device

--- test/include/io_test.rb
+++ test/include/io_test.rb
@@ -37,120 +37,226 @@ describe "Yast::AutoinstallIoInclude" do
                          "bank", "account", "pocket")).to eq(false)
     end
 
-    context "when scheme is 'device'" do
+    context "when scheme is 'device' or 'usb'" do
       let(:scheme) { "device" }
+
+      def expect_copy(from, to, result)
+        expect(Yast::WFM).to receive(:Execute)
+          .with(path(".local.bash"), "/bin/cp #{from} #{to}")
+          .and_return(result)
+      end
+
+      def expect_mount(device, mp, result)
+        expect(Yast::SCR).to receive(:Execute)
+          .with(path(".target.mount"),
+                [device, mp],
+                "-o noatime")
+          .and_return(result)
+      end
+
+      def expect_umount(mp, _result)
+        # Bug: local is wrong. nfs and cifs correctly use .target.umount
+        expect(Yast::WFM).to receive(:Execute)
+          .with(path(".local.umount"), mp)
+      end
+
+      def expect_mount_check(device, prior_mp)
+        allow(Yast::Storage).to receive(:GetUsedFs)
+        expect(Yast::Storage).to receive(:DeviceMounted)
+          .with(device).and_return(prior_mp)
+      end
+
+      def expect_is_directory?(device, result)
+        expect(Yast::SCR).to receive(:Read)
+          .with(path(".target.dir"), device)
+          .and_return(result ? [] : nil)
+      end
 
       it "returns false for an empty path" do
         expect(subject.Get(scheme, "sda", "", "/localfile")).to eq(false)
+      end
+
+      context "when host specifies a device" do
+        let(:host) { "sdc4" }
+        let(:localfile) { "/localfile" }
+
+        before do
+          expect(Yast::SCR).to receive(:Read)
+            .with(path(".target.dir"), "/dev/sdc4")
+            .and_return(nil)
+        end
+
+        it "checks + copies, successfully" do
+          expect_mount_check("/dev/sdc4", "/already_mounted")
+          expect_copy("/already_mounted/mypath", localfile, 0)
+
+          expect(subject.Get(scheme, host, "mypath", localfile))
+            .to eq(true)
+        end
+
+        it "checks + copies, failing" do
+          expect_mount_check("/dev/sdc4", "/already_mounted")
+          expect_copy("/already_mounted/mypath", localfile, 1)
+
+          expect(subject.Get(scheme, host, "mypath", localfile))
+            .to eq(false)
+        end
+
+        it "checks + mounts + copies + umounts, successfully" do
+          expect_mount_check("/dev/sdc4", "")
+          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", true)
+          expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 0)
+          expect_umount("/tmp_dir/tmp_mount", true)
+
+          expect(subject.Get(scheme, host, "mypath", localfile))
+            .to eq(true)
+        end
+
+        it "checks + fails to mount" do
+          expect_mount_check("/dev/sdc4", "")
+          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", false)
+
+          expect(subject.Get(scheme, host, "mypath", localfile))
+            .to eq(false)
+        end
+
+        it "checks + mounts + copies(fails) + umounts" do
+          expect_mount_check("/dev/sdc4", "")
+          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", true)
+          expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 1)
+          expect_umount("/tmp_dir/tmp_mount", true)
+
+          expect(subject.Get(scheme, host, "mypath", localfile))
+            .to eq(false)
+        end
+      end
+
+      context "when host+path specify a device" do
+        let(:host) { "disk" }
+        let(:urlpath) { "by-id/f00f/mypath" }
+        let(:localfile) { "/localfile" }
+
+        it "finds device + checks mount + copies, successfully" do
+          expect_is_directory?("/dev/disk", true)
+          expect_is_directory?("/dev/disk/by-id", true)
+          expect_is_directory?("/dev/disk/by-id/f00f", false)
+
+          expect_mount_check("/dev/disk/by-id/f00f", "/already_mounted")
+          expect_copy("/already_mounted/mypath", localfile, 0)
+
+          expect(subject.Get(scheme, host, urlpath, localfile))
+            .to eq(true)
+        end
       end
 
       context "when host is empty" do
         let(:host) { "" }
+        let(:urlpath) { "mypath" }
+        let(:localfile) { "/localfile" }
 
-        it "probes disks" do
-          probed_disks = [
-            {},
-            { "dev_name" => "/dev/sda" },
-            { "dev_name" => "/dev/sdb" }
-          ]
+        shared_examples "disk_prober" do
+          it "probes sda, sdb, and does lotsa stuff" do
+            lstat_mocks = {
+              "/dev/sda1" => true,
+              "/dev/sda2" => false,
+              "/dev/sda3" => false,
+              "/dev/sda4" => true,
+              "/dev/sda5" => true,
+              "/dev/sda6" => false,
 
-          expect(Yast::SCR).to receive(:Read)
-            .with(path(".probe.disk"))
-            .and_return(probed_disks)
+              "/dev/sdb1" => true,
+              "/dev/sdb2" => false,
+              "/dev/sdb3" => false,
+              "/dev/sdb4" => false,
+              "/dev/sdb5" => false
+            }
 
-          lstat_mocks = {
-            "/dev/sda1" => true,
-            "/dev/sda2" => false,
-            "/dev/sda3" => false,
-            "/dev/sda4" => true,
-            "/dev/sda5" => true,
-            "/dev/sda6" => false,
+            lstat_mocks.each do |device, exists|
+              expect(Yast::SCR).to receive(:Read)
+                .with(path(".target.lstat"), device).twice
+                .and_return(exists ? { "size" => 1 } : {})
+            end
 
-            "/dev/sdb1" => true,
-            "/dev/sdb2" => false,
-            "/dev/sdb3" => false,
-            "/dev/sdb4" => false,
-            "/dev/sdb5" => false
-          }
+            # only up to sda5 because that is when we find the file
+            mount_points = {
+              # device    => [prior mount point, temporary mount point]
+              "/dev/sda"  => ["",          "/tmp_dir/tmp_mount"],
+              "/dev/sda1" => ["/mnt_sda1", nil],
+              "/dev/sda4" => ["",          "/mnt_sda1"],
+              "/dev/sda5" => ["",          "/mnt_sda1"]
+            }
 
-          lstat_mocks.each do |device, exists|
-            expect(Yast::SCR).to receive(:Read)
-              .with(path(".target.lstat"), device).twice
-              .and_return(exists ? { "size" => 1 } : {})
+            mount_points.each do |device, mp|
+              expect_mount_check(device, mp.first)
+            end
+
+            # only up to sda5 because that is when we find the file
+            mount_succeeded = {
+              "/dev/sda"  => false,
+              "/dev/sda4" => true,
+              "/dev/sda5" => true,
+            }
+
+            mount_succeeded.each do |device, result|
+              expect_mount(device, mount_points[device].last, result)
+            end
+
+            # sda1 fails, sda4 fails, sda5 succeeds
+            expect_copy("/mnt_sda1/mypath", localfile, 1)
+            expect_copy("/mnt_sda1/mypath", localfile, 1)
+            expect_copy("/mnt_sda1/mypath", localfile, 0)
+
+            # if mount points are not reused, we should see
+            #  expect_copy("/tmp_dir/tmp_mount/mypath", localfile, ...)
+            # and aditionally if destdir is used properly,
+            #  expect_copy("/destdir/tmp_dir/tmp_mount/mypath", localfile, ...)
+
+            expect_umount("/mnt_sda1", true)
+            expect_umount("/mnt_sda1", true)
+
+            # DO IT, this is the call that needs all the above mocking
+            expect(subject.Get(scheme, host, "mypath", localfile))
+              .to eq(true)
           end
-
-          allow(Yast::SCR).to receive(:Dir)
-            .with(path(".product.features.section")).and_return([])
-
-          # only up to sda5 because that is when we find the file
-          mount_points = {
-            # device    => [prior mount point, temporary mount point]
-            "/dev/sda"  => ["",          "/tmp_dir/tmp_mount"],
-            "/dev/sda1" => ["/mnt_sda1", nil],
-            "/dev/sda4" => ["",          "/mnt_sda1"],
-            "/dev/sda5" => ["",          "/mnt_sda1"]
-          }
-
-          expect(Yast::Storage).to receive(:GetUsedFs).at_least(:once)
-
-          mount_points.each do |device, mp|
-            expect(Yast::Storage).to receive(:DeviceMounted)
-              .with(device).and_return(mp.first)
-          end
-
-          # only up to sda5 because that is when we find the file
-          mount_succeeded = {
-            "/dev/sda"  => false,
-            "/dev/sda4" => true,
-            "/dev/sda5" => true,
-          }
-
-          mount_succeeded.each do |device, result|
-            expect(Yast::SCR).to receive(:Execute)
-              .with(path(".target.mount"),
-                    [device, mount_points[device].last],
-                    "-o noatime")
-              .and_return(result)
-          end
-
-          expect(Yast::WFM).to receive(:Execute)
-            .with(path(".local.bash"),
-                  "/bin/cp /mnt_sda1/mypath /localfile")
-            .exactly(3).times
-            .and_return(1, 1, 0) # sda1 fails, sda4 fails, sda5 succeeds
-
-          expect(Yast::WFM).to receive(:Execute)
-            .with(path(".local.bash"),
-#                  "/bin/cp /destdir/tmp_dir/tmp_mount/mypath /localfile")
-# Bug: it is wrong if destdir is used
-                  "/bin/cp /tmp_dir/tmp_mount/mypath /localfile")
-            .exactly(0).times
-#            .and_return(1, 0)   # sda4 fails, sda5 succeeds
-
-# Bug: local is wrong. nfs and cifs correctly use .target.umount
-          expect(Yast::WFM).to receive(:Execute)
-            .with(path(".local.umount"), "/mnt_sda1")
-            .exactly(2).times
-
-          # DO IT, this is the call that needs all the above mocking
-          expect(subject.Get(scheme, host, "mypath", "/localfile"))
-            .to eq(true)
         end
 
-      end
+        context "when scheme is 'device'" do
+          let(:scheme) { "device" }
 
-      context "when host specifies a device" do
-      end
+          before do
+            probed_disks = [
+              {},
+              { "dev_name" => "/dev/sda" },
+              { "dev_name" => "/dev/sdb" }
+            ]
 
-      context "when host+path specify a device" do
-      end
-    end
+            expect(Yast::SCR).to receive(:Read)
+              .with(path(".probe.disk"))
+              .and_return(probed_disks)
+          end
 
-    context "when scheme is 'usb'" do
-      let(:scheme) { "usb" }
+          it_should_behave_like "disk_prober"
+        end
 
-      it "returns false for an empty path" do
-        expect(subject.Get(scheme, "sda", "", "/localfile")).to eq(false)
+        context "when scheme is 'usb'" do
+          let(:scheme) { "usb" }
+
+          before do
+            probed_disks = [
+              {},
+              { "dev_name" => "/dev/minnie" },
+              { "dev_name" => "/dev/mickey", "bus" => "USB"  },
+              { "dev_name" => "/dev/sda",    "bus" => "SCSI" },
+              { "dev_name" => "/dev/sdb",    "bus" => "SCSI" }
+            ]
+
+            expect(Yast::SCR).to receive(:Read)
+              .with(path(".probe.usb"))
+              .and_return(probed_disks)
+          end
+
+          it_should_behave_like "disk_prober"
+        end
       end
     end
 
