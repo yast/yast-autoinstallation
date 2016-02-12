f0b45eeeea6d93e0a348b4664d51aa1191576aab
Martin Vidner <mvidner@suse.cz>
2016-02-12 15:41:11 +0100

    Test AutoinstallIoInclude#Get floppy, tftp

--- test/include/io_test.rb
+++ test/include/io_test.rb
@@ -14,22 +14,61 @@ describe "Yast::AutoinstallIoInclude" do
   subject { AutoinstallIoTest.new }
 
   describe "#Get" do
+    let(:tmpdir) { "/tmp_dir" }
+    let(:mount_point) { "#{tmpdir}/tmp_mount" }
+    let(:destdir) { "/destdir" }
+
+    def expect_copy(from, to, result)
+      expect(Yast::WFM).to receive(:Execute)
+        .with(path(".local.bash"), "/bin/cp #{from} #{to}")
+        .and_return(result ? 0 : 1)
+    end
+
+    def expect_mount_generic(local_or_target, device, mp, result, optstring)
+      args = [path(".#{local_or_target}.mount"), [device, mp]]
+      args << optstring unless optstring.nil?
+      receiver = (local_or_target == "local") ? Yast::WFM : Yast::SCR
+      expect(receiver).to receive(:Execute)
+        .with(*args)
+        .and_return(result)
+    end
+
+    def expect_mount(device, mp, result, optstring = nil)
+      expect_mount_generic("target", device, mp, result, optstring)
+    end
+
+    # Some code paths use a different method which seems to be wrong
+    # The local/target mess was last modified in
+    # https://github.com/yast/yast-autoinstallation/commit/69f1966dd1456301a69102c6d3bacfe7c9f9dc49
+    # for https://bugzilla.suse.com/show_bug.cgi?id=829265
+    def expect_mount_local(device, mp, result, optstring = nil)
+      expect_mount_generic("local", device, mp, result, optstring)
+    end
+
+    def expect_umount(mp, _result)
+      expect(Yast::SCR).to receive(:Execute)
+        .with(path(".target.umount"), mp)
+    end
+
+    def expect_umount_local(mp, _result)
+      # Bug: local is wrong. nfs and cifs correctly use .target.umount
+      expect(Yast::WFM).to receive(:Execute)
+        .with(path(".local.umount"), mp)
+    end
+
     before do
       expect(Yast::AutoinstConfig).to receive(:urltok)
         .and_return({})
       expect(Yast::WFM).to receive(:Read)
-        .and_return("/tmp_dir")
+        .and_return(tmpdir)
       expect(Yast::WFM).to receive(:SCRGetDefault)
         .and_return(333)
       expect(Yast::WFM).to receive(:SCRGetName).with(333)
         .and_return("chroot=/mnt:scr")
       expect(Yast::AutoinstConfig).to receive(:destdir)
-        .and_return("/destdir")
+        .and_return(destdir)
       expect(Yast::WFM).to receive(:Execute)
-        .with(path(".local.mkdir"), "/destdir/tmp_dir/tmp_mount")
-      # the local/target mess was last modified in
-      # https://github.com/yast/yast-autoinstallation/commit/69f1966dd1456301a69102c6d3bacfe7c9f9dc49
-      # for https://bugzilla.suse.com/show_bug.cgi?id=829265
+        .with(path(".local.mkdir"), "#{destdir}#{mount_point}")
     end
 
     it "returns false for unknown scheme" do
@@ -39,26 +78,6 @@ describe "Yast::AutoinstallIoInclude" do
 
     context "when scheme is 'device' or 'usb'" do
       let(:scheme) { "device" }
-
-      def expect_copy(from, to, result)
-        expect(Yast::WFM).to receive(:Execute)
-          .with(path(".local.bash"), "/bin/cp #{from} #{to}")
-          .and_return(result)
-      end
-
-      def expect_mount(device, mp, result)
-        expect(Yast::SCR).to receive(:Execute)
-          .with(path(".target.mount"),
-                [device, mp],
-                "-o noatime")
-          .and_return(result)
-      end
-
-      def expect_umount(mp, _result)
-        # Bug: local is wrong. nfs and cifs correctly use .target.umount
-        expect(Yast::WFM).to receive(:Execute)
-          .with(path(".local.umount"), mp)
-      end
 
       def expect_mount_check(device, prior_mp)
         allow(Yast::Storage).to receive(:GetUsedFs)
@@ -88,7 +107,7 @@ describe "Yast::AutoinstallIoInclude" do
 
         it "checks + copies, successfully" do
           expect_mount_check("/dev/sdc4", "/already_mounted")
-          expect_copy("/already_mounted/mypath", localfile, 0)
+          expect_copy("/already_mounted/mypath", localfile, true)
 
           expect(subject.Get(scheme, host, "mypath", localfile))
             .to eq(true)
@@ -96,7 +115,7 @@ describe "Yast::AutoinstallIoInclude" do
 
         it "checks + copies, failing" do
           expect_mount_check("/dev/sdc4", "/already_mounted")
-          expect_copy("/already_mounted/mypath", localfile, 1)
+          expect_copy("/already_mounted/mypath", localfile, false)
 
           expect(subject.Get(scheme, host, "mypath", localfile))
             .to eq(false)
@@ -104,9 +123,9 @@ describe "Yast::AutoinstallIoInclude" do
 
         it "checks + mounts + copies + umounts, successfully" do
           expect_mount_check("/dev/sdc4", "")
-          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", true)
-          expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 0)
-          expect_umount("/tmp_dir/tmp_mount", true)
+          expect_mount("/dev/sdc4", mount_point, true, "-o noatime")
+          expect_copy("#{mount_point}/mypath", localfile, true)
+          expect_umount_local(mount_point, true)
 
           expect(subject.Get(scheme, host, "mypath", localfile))
             .to eq(true)
@@ -114,7 +133,7 @@ describe "Yast::AutoinstallIoInclude" do
 
         it "checks + fails to mount" do
           expect_mount_check("/dev/sdc4", "")
-          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", false)
+          expect_mount("/dev/sdc4", mount_point, false, "-o noatime")
 
           expect(subject.Get(scheme, host, "mypath", localfile))
             .to eq(false)
@@ -122,9 +141,9 @@ describe "Yast::AutoinstallIoInclude" do
 
         it "checks + mounts + copies(fails) + umounts" do
           expect_mount_check("/dev/sdc4", "")
-          expect_mount("/dev/sdc4", "/tmp_dir/tmp_mount", true)
-          expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 1)
-          expect_umount("/tmp_dir/tmp_mount", true)
+          expect_mount("/dev/sdc4", mount_point, true, "-o noatime")
+          expect_copy("#{mount_point}/mypath", localfile, false)
+          expect_umount_local(mount_point, true)
 
           expect(subject.Get(scheme, host, "mypath", localfile))
             .to eq(false)
@@ -142,7 +161,7 @@ describe "Yast::AutoinstallIoInclude" do
           expect_is_directory?("/dev/disk/by-id/f00f", false)
 
           expect_mount_check("/dev/disk/by-id/f00f", "/already_mounted")
-          expect_copy("/already_mounted/mypath", localfile, 0)
+          expect_copy("/already_mounted/mypath", localfile, true)
 
           expect(subject.Get(scheme, host, urlpath, localfile))
             .to eq(true)
@@ -197,19 +216,19 @@ describe "Yast::AutoinstallIoInclude" do
             }
 
             mount_succeeded.each do |device, result|
-              expect_mount(device, "/tmp_dir/tmp_mount", result)
+              expect_mount(device, mount_point, result, "-o noatime")
             end
 
             # sda1 fails, sda4 fails, sda5 succeeds
-            expect_copy("/mnt_sda1/mypath", localfile, 1)
-            expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 1)
-            expect_copy("/tmp_dir/tmp_mount/mypath", localfile, 0)
+            expect_copy("/mnt_sda1/mypath", localfile, false)
+            expect_copy("#{mount_point}/mypath", localfile, false)
+            expect_copy("#{mount_point}/mypath", localfile, true)
             # if destdir is used properly, we should see
-            #  expect_copy("/destdir/tmp_dir/tmp_mount/mypath", localfile, ...)
+            #  expect_copy("/destdir#{mount_point}/mypath", localfile, ...)
 
             mount_succeeded.each do |device, result|
               # if the MOUNT succeeded previously, now UMOUNT
-              expect_umount("/tmp_dir/tmp_mount", true) if result
+              expect_umount_local(mount_point, true) if result
             end
 
             # DO IT, this is the call that needs all the above mocking
@@ -269,9 +288,73 @@ describe "Yast::AutoinstallIoInclude" do
     end
     context "when scheme is 'cifs'" do
     end
-    context "when scheme is 'floppy'" do
-    end
+
     context "when scheme is 'tftp'" do
+      let(:scheme) { "tftp" }
+      let(:host) { "example.com" }
+      let(:urlpath) { "/foo" }
+      let(:localfile) { "/localfile" }
+
+      it "delegates to TFTP.GET, with success" do
+        expect(Yast::TFTP).to receive(:Get)
+          .with(host, urlpath, localfile)
+          .and_return(true)
+
+        expect(subject.Get(scheme, host, urlpath, localfile))
+          .to eq(true)
+      end
+
+      it "delegates to TFTP.GET, with failure" do
+        expect(Yast::TFTP).to receive(:Get)
+          .with(host, urlpath, localfile)
+          .and_return(false)
+
+        expect(subject.Get(scheme, host, urlpath, localfile))
+          .to eq(false)
+      end
     end
+
+    context "when scheme is 'floppy'" do
+      let(:scheme) { "floppy" }
+      let(:host) { "unused" }
+      let(:urlpath) { "/foo" }
+      let(:localfile) { "/localfile" }
+
+      it "fails if floppy is not ready" do
+        expect(Yast::StorageDevices).to receive(:FloppyReady)
+          .and_return(false)
+        expect(subject.Get(scheme, host, urlpath, localfile))
+          .to eq(false)
+      end
+
+      context "when floppy is ready" do
+        let(:device) { "/dev/fd7" }
+        before do
+          expect(Yast::StorageDevices).to receive(:FloppyReady)
+            .and_return(true)
+          expect(Yast::StorageDevices).to receive(:FloppyDevice)
+            .and_return(device)
+        end
+
+        it "mounts, copies successfully, umounts" do
+          expect_mount_local(device, mount_point, true)
+          expect_copy("#{mount_point}/#{urlpath}", localfile, true)
+          expect_umount(mount_point, true)
+
+          expect(subject.Get(scheme, host, urlpath, localfile))
+            .to eq(true)
+        end
+
+        it "mounts, copies unsuccessfully, umounts" do
+          expect_mount_local(device, mount_point, true)
+          expect_copy("#{mount_point}/#{urlpath}", localfile, false)
+          expect_umount(mount_point, true)
+
+          expect(subject.Get(scheme, host, urlpath, localfile))
+            .to eq(false)
+        end
+      end
+    end
+
   end
 end
