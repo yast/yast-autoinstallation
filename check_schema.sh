3f196fcd550a7f513c15f59ec2cc4a549758c0e2
Ladislav Slezák <lslezak@suse.cz>
2017-01-05 15:37:18 +0100

    Improve the check_schema.sh script
    - check that "trang" is actually installed
    - be more verbose (print progress and the result)

--- check_schema.sh
+++ check_schema.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 
+# This script checks that all RNC files can be converted to RNG without errors.
+
+# Fail if trang is missing
+if ! which trang > /dev/null; then
+  echo 'ERROR: "trang" tool is missing'
+  exit 1
+fi
+
 # explicitly check the RNC schema files for errors
 # (the files are defined here, but converted in yast2-schema)
-find . -name "*.rnc" -exec trang -I rnc -O rng \{\} test.rng \; 2> trang.log
-rm -f test.rng
+find . -name "*.rnc" -exec sh -c "echo 'Checking {}...'; trang -I rnc -O rng {} /dev/null" \; 2> trang.log
 
 # grep for "error" in the output, "trang" returns 0 exit status
 # even on an error :-(
@@ -15,4 +22,4 @@ if grep -i -q error trang.log; then
 fi
 
 rm -f trang.log
-
+echo "OK"
