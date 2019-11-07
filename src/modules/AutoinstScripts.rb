bf462829702c17e137a88d93db9bea1ddd792280
Josef Reidinger <jreidinger@suse.cz>
2019-11-07 10:16:44 +0100

    report wrong type of param-list (bsc#1143260)

--- src/modules/AutoinstScripts.rb
+++ src/modules/AutoinstScripts.rb
@@ -727,6 +727,11 @@ module Yast
 
       Builtins.foreach(scripts) do |s|
         scriptInterpreter = Ops.get_string(s, "interpreter", "shell")
+        params = s.fetch("param-list", [])
+        if !params.is_a?(::Array)
+          Report.Error("Element param-list does not have config-type list specified. Skipping it.")
+          params = []
+        end
         params = s.fetch("param-list", []).join(" ")
         scriptName = Ops.get_string(s, "filename", "")
         if scriptName == ""
