c1f58b52336311acebba62f8fbecb032dee9c357
David Díaz González <dgonzalez@suse.de>
2020-04-29 15:05:51 +0100

    Update VgExtentSize widget
    Which now is a SizeSelector

--- src/lib/autoinstall/widgets/storage/vg_extent_size.rb
+++ src/lib/autoinstall/widgets/storage/vg_extent_size.rb
@@ -19,60 +19,29 @@
 
 require "yast"
 require "y2storage"
-require "cwm/common_widgets"
+require "autoinstall/widgets/storage/size_selector"
 
 module Y2Autoinstallation
   module Widgets
     module Storage
       # Widget for the LVM VG extent size
-      class VgExtentSize < CWM::ComboBox
-        SUGGESTED_SIZES = ["1 MiB", "2 MiB", "4 MiB", "8 MiB", "16 MiB", "32 MiB", "64 MiB"].freeze
-
-        # Constructor
-        def initalize
-          textdomain "autoinst"
-          super
-        end
-
-        def opt
-          [:editable]
-        end
-
+      class VgExtentSize < SizeSelector
         # @macro seeAbstractWidget
         def label
           # TRANSLATORS: field to enter the extent size of a new volume group
           _("Physical Extent Size")
         end
 
-        # Returns selected size
-        #
-        # @return [String] a human readable disk size
-        def value
-          Y2Storage::DiskSize.from_s(super).to_s
+        def sizes
+          ["1 MiB", "2 MiB", "4 MiB", "8 MiB", "16 MiB", "32 MiB", "64 MiB"]
         end
 
-        # Updates the value with a human readable version of given size
-        #
-        # @param size [#to_s]
-        def value=(size)
-          return if size.nil?
-
-          super(Y2Storage::DiskSize.from_s(size.to_s).to_s)
+        def include_max?
+          false
         end
 
-        # @macro seeAbstractWidget
-        def help
-          # TRANSLATORS: help text
-          _("<p><b>Physical Extent Size:</b> " \
-            "The smallest size unit used for volumes. " \
-            "This cannot be changed after creating the volume group. " \
-            "You can resize a logical volume only in multiples of this size." \
-            "</p>")
-        end
-
-        # @macro seeAbstractWidget
-        def items
-          SUGGESTED_SIZES.map { |mp| [mp, mp] }
+        def include_auto?
+          false
         end
       end
     end
