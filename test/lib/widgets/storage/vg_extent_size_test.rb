0bc900cc2ffabdedeaf3ab5d3e3c5f2f56bbd3d5
David Díaz González <dgonzalez@suse.de>
2020-04-29 15:05:51 +0100

    Update VgExtentSize widget
    Which now is a SizeSelector

--- test/lib/widgets/storage/vg_extent_size_test.rb
+++ test/lib/widgets/storage/vg_extent_size_test.rb
@@ -25,37 +25,4 @@ describe Y2Autoinstallation::Widgets::Storage::VgExtentSize do
   subject(:widget) { described_class.new }
 
   include_examples "CWM::ComboBox"
-
-  describe "#value=" do
-    context "when size is given" do
-      let(:size)  { 67108864 }
-
-      it "updates the extent size with its human readable version" do
-        expect(Yast::UI).to receive(:ChangeWidget).with(Id(widget.widget_id), :Value, "64 MiB")
-
-        widget.value = size
-      end
-    end
-
-    context "when size is not given" do
-      let(:size)  { nil }
-
-      it "does nothing" do
-        expect(Yast::UI).to_not receive(:ChangeWidget)
-
-        widget.value = size
-      end
-    end
-  end
-
-  describe "#value" do
-    before do
-      allow(Yast::UI).to receive(:QueryWidget).with(Id(widget.widget_id), :Value)
-        .and_return("67108864")
-    end
-
-    it "returns a human readable extent size" do
-      expect(widget.value).to eq("64 MiB")
-    end
-  end
 end
