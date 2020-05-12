d861ccb5b0ffa531ef6ac70a3f8e6795fed6e887
David Díaz González <dgonzalez@suse.de>
2020-05-12 09:43:06 +0100

    Split partition section page in two tabs
    A partition section could have three groups of options
    
      * those that are always (create, size, etc)
      * others that depends on the drive section type (e.g., LVM name)
      * options related to the partition usage (filesystem, LVM group, etc)
    
    which have been organized in two tabs, one for the two first groups and
    other for the last one.
    
    That way, the interface will look less crowded.

---
+++ src/lib/autoinstall/widgets/storage/partition_usage_tab.rb
@@ -0,0 +1,123 @@
+# Copyright (c) [2020] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "cwm/tabs"
+require "cwm/replace_point"
+require "cwm/common_widgets"
+require "autoinstall/widgets/storage/filesystem_attrs"
+require "autoinstall/widgets/storage/raid_attrs"
+require "autoinstall/widgets/storage/lvm_pv_attrs"
+require "autoinstall/widgets/storage/lvm_partition_attrs"
+require "autoinstall/widgets/storage/size_selector"
+require "autoinstall/widgets/storage/used_as"
+
+module Y2Autoinstallation
+  module Widgets
+    module Storage
+      # Tab to manage the partition section options that depends on its usage (file system, LVM PV,
+      # RAID member, etc).
+      class PartitionUsageTab < ::CWM::Tab
+        # Constructor
+        #
+        # @param partition [Presenters::Partition] presenter for a partition section of the profile
+        def initialize(partition)
+          textdomain "autoinst"
+
+          @partition = partition
+          self.handle_all_events = true
+        end
+
+        # @macro seeAbstractWidget
+        def label
+          # TRANSLATORS: name of the tab to display the partition section options based on its usage
+          _("Used as")
+        end
+
+        def contents
+          VBox(
+            Left(used_as_widget),
+            replace_point
+          )
+        end
+
+        # @macro seeAbstractWidget
+        def init
+          used_as_widget.value = partition.usage.to_s
+          update_replace_point
+        end
+
+        # @macro seeAbstractWidget
+        def handle(event)
+          update_replace_point if event["ID"] == "used_as"
+          nil
+        end
+
+        # @macro seeAbstractWidget
+        def store
+          partition.update(values, attrs: filesystem_widget.values.keys)
+          nil
+        end
+
+        # @macro seeAbstractWidget
+        def values
+          selected_widget.values
+        end
+
+      private
+
+        # @return [Presenters::Partition] presenter for the partition section
+        attr_reader :partition
+
+        def used_as_widget
+          @used_as_widget ||= UsedAs.new
+        end
+
+        def filesystem_widget
+          @filesystem_widget ||= FilesystemAttrs.new(partition)
+        end
+
+        def raid_widget
+          @raid_widget ||= RaidAttrs.new(partition)
+        end
+
+        def lvm_pv_widget
+          @lvm_pv_widget ||= LvmPvAttrs.new(partition)
+        end
+
+        def replace_point
+          @replace_point ||= CWM::ReplacePoint.new(id: "attrs", widget: filesystem_widget)
+        end
+
+        # Updates the replace point with the content corresponding widget for selected type
+        def update_replace_point
+          replace_point.replace(selected_widget)
+        end
+
+        # Returns the selected widget according to the UsedAs widget
+        #
+        # @return [CWM::AbstractWidget]
+        def selected_widget
+          send("#{used_as_widget.value}_widget")
+        end
+      end
+    end
+  end
+end
+
