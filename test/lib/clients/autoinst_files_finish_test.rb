3705b20946ca8545dacf6d7533ba68f071243dac
Stefan Schubert <schubi@suse.de>
2020-07-16 13:14:54 +0200

    testcase

--- test/lib/clients/autoinst_files_finish_test.rb
+++ test/lib/clients/autoinst_files_finish_test.rb
@@ -1,8 +1,13 @@
 require_relative "../../test_helper"
 require "autoinstall/clients/autoinst_files_finish"
 
+Yast.import "Mode"
+
 describe Y2Autoinstallation::Clients::AutoinstFilesFinish do
   describe "#write" do
+    before do
+      allow(Yast::Mode).to receive(:auto).and_return true
+    end
     it "writes additional configuration files" do
       expect(Yast::AutoinstFile).to receive(:Write)
       subject.write
