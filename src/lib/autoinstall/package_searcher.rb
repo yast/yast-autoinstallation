c9c8d44b78e9df3df0511e06ec181296a2fe8a47
Stefan Schubert <schubi@suse.de>
2020-08-07 14:00:38 +0200

    fix in last_match

--- src/lib/autoinstall/package_searcher.rb
+++ src/lib/autoinstall/package_searcher.rb
@@ -50,7 +50,7 @@ module Y2Autoinstallation
             next if !dep["supplements"] ||
               !dep["supplements"].match?(/^autoyast\((.*)\)/)
 
-            suppl_sections = Regexp.last_match[1].split(",")
+            suppl_sections = Regexp.last_match.split(",")
             suppl_sections.each do |sup_section|
               next unless sup_section == section
 
