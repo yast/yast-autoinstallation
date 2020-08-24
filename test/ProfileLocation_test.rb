b0e3feb7aa8df52449345005e09e4e760211e632
Stefan Schubert <schubi@suse.de>
2020-08-24 14:08:05 +0200

    added testcase

--- test/ProfileLocation_test.rb
+++ test/ProfileLocation_test.rb
@@ -28,5 +28,26 @@ describe "Yast::ProfileLocation" do
         subject.Process
       end
     end
+
+    context "when scheme is corrupted" do
+      before do
+        Yast::AutoinstConfig.scheme = "relurl"
+        Yast::AutoinstConfig.xml_tmpfile = "/tmp/123"
+        Yast::AutoinstConfig.filepath = "error.xml"
+        allow(Yast::InstURL).to receive(:installInf2Url).and_return(
+          "http://download.opensuse.org/distribution/leap/15.1/repo/oss/"
+        )
+        allow(subject).to receive(:Get).with("http",
+          "download.opensuse.org",
+          "/distribution/leap/15.1/repo/oss/autoinst.xml",
+          "/tmp/123").and_return("error_string")
+      end
+
+      it "reports an error" do
+        expect_any_instance_of(String).to receive(:valid_encoding?).and_return(false)
+        expect(Yast::Report).to receive(:Error).with(/has no valid encoding or is corrupted/)
+        subject.Process
+      end
+    end
   end
 end
