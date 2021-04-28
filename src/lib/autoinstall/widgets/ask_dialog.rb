e7be0813e3399aa82884a4221d4980bc6ae585fe
Imobach González Sosa <igonzalezsosa@suse.com>
2021-04-28 21:09:46 +0100

    Add a widget to display an <ask> dialog

---
+++ src/lib/autoinstall/widgets/ask_dialog.rb
@@ -0,0 +1,213 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "cwm/dialog"
+require "cwm/common_widgets"
+
+module Y2Autoinstall
+  module Widgets
+    # Implements a CWM dialog for the {Y2Autoinstall::Ask::Dialog} class
+    #
+    # The dialog displays all the questions included in the dialog and updates
+    # their values accordingly.
+    #
+    # @example 'Running' a dialog
+    #   question = Y2Autoinstall::Ask::Question.new("Username")
+    #   dialog = Y2Autoinstall::Ask::Dialog.new(1, [question])
+    #   AskDialog.new(dialog).run
+    class AskDialog < CWM::Dialog
+      # Common methods for question widgets
+      module AskWidget
+        # Returns the widget_id, which is based in the type and the question's object_id
+        #
+        # @return [String] Widget ID
+        def widget_id
+          "#{widget_type}_#{@question.object_id}"
+        end
+
+        # Returns the widget's label
+        #
+        # The widget's label corresponds to the question text.
+        #
+        # @return [String]
+        # @macro seeAbstractWidget
+        def label
+          @question.text
+        end
+
+        # Initializes the widget value
+        #
+        # Use the question's value or the default one.
+        # @macro seeAbstractWidget
+        def init
+          self.value = @question.value || @question.default
+        end
+
+        # Stores the widget's value in the question
+        #
+        # @macro seeAbstractWidget
+        def store
+          @question.value = value
+        end
+      end
+
+      # CheckBox widget for a question
+      class CheckBox < CWM::CheckBox
+        include AskWidget
+
+        # @param question [Y2Autoinstall::Ask::Question] Question to represent
+        def initialize(question)
+          @question = question
+        end
+
+        # @macro seeAbstractWidget
+        def opt
+          [:notify]
+        end
+
+        # @macro seeAbstractWidget
+        def init
+          checked = @question.value.nil? ? @question.default == "true" : @question.value
+          self.value = checked
+        end
+      end
+
+      class InputField < CWM::InputField
+        include AskWidget
+
+        # @param question [Y2Autoinstall::Ask::Question] Question to represent
+        def initialize(question)
+          @question = question
+        end
+
+        # @macro seeAbstractWidget
+        def opt
+          [:hstretch, :notify, :notifyContextMenu]
+        end
+      end
+
+      class Password < CWM::InputField
+        include AskWidget
+
+        # @param question [Y2Autoinstall::Ask::Question] Question to represent
+        def initialize(question)
+          @question = question
+        end
+
+        # @macro seeAbstractWidget
+        def opt
+          [:notify, :notifyContextMenu]
+        end
+      end
+
+      class ComboBox < CWM::ComboBox
+        include AskWidget
+
+        # @param question [Y2Autoinstall::Ask::Question] Question to represent
+        def initialize(question)
+          @question = question
+        end
+
+        # @macro seeAbstractWidget
+        def opt
+          [:notify, :immediate]
+        end
+
+        # @macro seeComboBox
+        def items
+          @question.options.map do |option|
+            label = option.label || option.value
+            [option.value, label]
+          end
+        end
+      end
+
+      # @macro seeDialog
+      attr_reader :disable_buttons
+
+      # Constructor
+      #
+      # @param dialog [Y2Autoinstall::Ask::Dialog] Dialog specification to display
+      # @param disable_back_button [Boolean] Whether the :back button should be disabled
+      def initialize(dialog, disable_back_button: false)
+        @dialog = dialog
+        @disable_buttons = disable_back_button ? ["back_button"] : []
+      end
+
+      # @macro seeAbstractWidget
+      def title
+        dialog.title
+      end
+
+      # @macro seeAbstractWidget
+      def contents
+        widgets = dialog.questions.each_with_index.map { |q| widget_for(q) }
+        VBox(*widgets)
+      end
+
+      # @macro seeDialog
+      def run
+        result = super
+        dialog.questions.each { |q| log.debug "#{q.text} -> #{q.value}" }
+        result
+      end
+
+    private
+
+      attr_reader :dialog
+
+      # Returns the proper widget for the given question
+      #
+      # @param question [Y2Autoinstall::Ask::Question]
+      def widget_for(question)
+        if question.type == "boolean"
+          CheckBox.new(question)
+        elsif question.type == "static_text"
+          Label(Id("label_#{question.object_id}"), question.default)
+        elsif question.password
+          Password.new(question)
+        elsif !question.options.empty?
+          ComboBox.new(question)
+        else
+          InputField.new(question)
+        end
+      end
+
+      # @macro seeDialog
+      def abort_button
+        ""
+      end
+
+      # @macro seeDialog
+      def back_button
+        return dialog.back_label if dialog.back_label
+
+        super
+      end
+
+      # @macro seeDialog
+      def next_button
+        return dialog.ok_label if dialog.ok_label
+
+        super
+      end
+    end
+  end
+end
---
+++ test/lib/autoinstall/widgets/ask_dialog_test.rb
@@ -0,0 +1,98 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../../test_helper"
+require "autoinstall/widgets/ask_dialog"
+require "autoinstall/ask/dialog"
+require "autoinstall/ask/question"
+require "autoinstall/ask/question_option"
+
+describe Y2Autoinstall::Widgets::AskDialog do
+  def widgets_from(term)
+    term.params
+  end
+
+  subject { described_class.new(dialog) }
+
+  let(:dialog) { Y2Autoinstall::Ask::Dialog.new(1, []) }
+
+  describe "#contents" do
+    let(:dialog) { Y2Autoinstall::Ask::Dialog.new(1, [question]) }
+
+    context "when the dialog includes a text field" do
+      let(:question) do
+        Y2Autoinstall::Ask::Question.new("Question 1")
+      end
+
+      it "includes an input field" do
+        widget = widgets_from(subject.contents).first
+        expect(widget).to be_a(Y2Autoinstall::Widgets::AskDialog::InputField)
+        expect(widget.label).to eq("Question 1")
+      end
+    end
+
+    context "when the dialog includes a boolean question" do
+      let(:question) do
+        Y2Autoinstall::Ask::Question.new("Question 1").tap do |q|
+          q.type = "boolean"
+        end
+      end
+
+      it "includes a checkbox" do
+        widget = widgets_from(subject.contents).first
+        expect(widget).to be_a(Y2Autoinstall::Widgets::AskDialog::CheckBox)
+        expect(widget.label).to eq("Question 1")
+      end
+    end
+
+    context "when the dialog includes an static text" do
+      let(:question) do
+        Y2Autoinstall::Ask::Question.new("Question 1").tap do |q|
+          q.default = "Question 1"
+          q.type = "static_text"
+        end
+      end
+
+      it "includes a label" do
+        widget = widgets_from(subject.contents).first
+        expect(widget.value).to eq(:Label)
+        _id, text = widget.to_a
+        expect(text).to eq("Question 1")
+      end
+    end
+
+    context "when the question includes some options to choose from" do
+      let(:question) do
+        Y2Autoinstall::Ask::Question.new("Question 1").tap do |q|
+          q.options = [
+            Y2Autoinstall::Ask::QuestionOption.new("opt1", "Option 1"),
+            Y2Autoinstall::Ask::QuestionOption.new("opt2", "Option 2")
+          ]
+        end
+      end
+
+      it "includes a ComboBox" do
+        widget = widgets_from(subject.contents).first
+        expect(widget).to be_a(Y2Autoinstall::Widgets::AskDialog::ComboBox)
+        texts = widget.items.map(&:to_a).map(&:last)
+        expect(texts).to eq(["Option 1", "Option 2"])
+      end
+    end
+  end
+end
