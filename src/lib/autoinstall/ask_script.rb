b7d481a0faf59959df4c0c0c5272d5db63c0be60
Imobach González Sosa <igonzalezsosa@suse.com>
2021-04-29 13:18:39 +0100

    Add an AskScript class to handle scripts in <ask> sections

---
+++ src/lib/autoinstall/ask_script.rb
@@ -0,0 +1,34 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "autoinstall/script"
+
+module Y2Autoinstall
+  # Scripts that are used when processing the <ask-list> section
+  class AskScript < Y2Autoinstallation::ExecutedScript
+    def self.type
+      "ask-script"
+    end
+
+    # Filename of script, used as ID
+    def filename
+      @filename || "ask-script"
+    end
+  end
+end
