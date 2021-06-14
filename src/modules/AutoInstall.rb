yast/yast-autoinstallation

commit e9fb94b3f3b2f963a726a509174e3a2d2b51ee7a

Copy all ask-* scripts at the end of the installation

src/modules/AutoInstall.rb
@@ -224,17 +224,20 @@ module Yast
       # copying ask-scripts and corresponding log files
       # to /var/adm/autoinstall
       SCR.Execute(path(".target.bash"),
-        "/bin/cp #{AutoinstConfig.tmpDir}/ask_scripts_log/*"\
+        "/bin/cp #{AutoinstConfig.tmpDir}/ask-scripts/logs/*"\
         " #{destdir}#{AutoinstConfig.logs_dir}")
       SCR.Execute(path(".target.bash"),
-        "/bin/cp #{AutoinstConfig.tmpDir}/*.sh"\
+        "/bin/cp #{AutoinstConfig.tmpDir}/ask-scripts/*"\
+        " #{destdir}#{AutoinstConfig.scripts_dir}")
+      SCR.Execute(path(".target.bash"),
+        "/bin/cp #{AutoinstConfig.tmpDir}/ask-default-value-scripts/*"\
         " #{destdir}#{AutoinstConfig.scripts_dir}")
 
       SCR.Execute(
         path(".target.bash"),
         Builtins.sformat(
           "/bin/cp %1 %2%3",
-          "/tmp/profile/autoinst.xml",
+          "#{AutoinstConfig.tmpDir}/profile/autoinst.xml",
           destdir,
           AutoinstConfig.xml_file
         )

test/AutoInstall_test.rb
@@ -4,6 +4,7 @@ require_relative "test_helper"
 require "installation/autoinst_issues"
 require "autoinstall/dialogs/question"
 require "installation/autoinst_profile/section_with_attributes"
+require "tmpdir"
 
 Yast.import "AutoInstall"
 Yast.import "UI"
@@ -96,6 +97,41 @@ describe "Yast::AutoInstall" do
           .and_return(:ok)
         expect(subject.valid_imported_values).to eq(true)
       end
+    end
+  end
+
+  describe "#Finish" do
+    let(:destdir) { Dir.mktmpdir }
+    let(:logs_dir) { "/logs" }
+    let(:scripts_dir) { "/scripts" }
+
+    before do
+      allow(Yast::AutoinstConfig).to receive(:tmpDir)
+        .and_return(FIXTURES_PATH.join("instsys", "tmp").to_s)
+      allow(Yast::AutoinstConfig).to receive(:logs_dir).and_return(logs_dir)
+      allow(Yast::AutoinstConfig).to receive(:scripts_dir).and_return(scripts_dir)
+      FileUtils.mkdir(File.join(destdir, logs_dir))
+      FileUtils.mkdir(File.join(destdir, scripts_dir))
+      FileUtils.mkdir_p(File.join(destdir, "var", "adm", "autoinstall", "cache"))
+    end
+
+    after do
+      FileUtils.remove_entry(destdir) if Dir.exist?(destdir)
+    end
+
+    it "copies scripts files" do
+      subject.Finish(destdir)
+
+      expect(File).to exist(File.join(destdir, "scripts", "ask.sh"))
+      expect(File).to exist(File.join(destdir, "scripts", "ask-value.sh"))
+      expect(File).to exist(File.join(destdir, "scripts", "pre.sh"))
+
+      expect(File).to exist(File.join(destdir, "logs", "ask.sh.log"))
+      expect(File).to exist(File.join(destdir, "logs", "pre.sh.log"))
+
+      expect(File).to exist(
+        File.join(destdir, "var", "adm", "autoinstall", "cache", "installedSystem.xml")
+      )
     end
   end
 end

src/lib/autoinstall/script.rb
@@ -470,9 +470,7 @@ module Y2Autoinstallation
     def execute
       cmd = INTERPRETER_MAP[interpreter] || interpreter
 
-      Yast::Execute.on_target(
-        cmd, script_path.shellescape, stdout: :capture
-      )
+      Yast::Execute.on_target(cmd, script_path.shellescape, stdout: :capture)
     end
   end
 

test/.gitignore
@@ -1,2 +1,0 @@
-*.log
-*.trs

commit 17736e44e5ae62f06a605d21a5991496cd97d4ea

Copy the profile to the installed system
* This feature got broken in e9fb94b3.

src/modules/AutoInstall.rb
@@ -237,7 +237,7 @@ module Yast
         path(".target.bash"),
         Builtins.sformat(
           "/bin/cp %1 %2%3",
-          "#{AutoinstConfig.tmpDir}/profile/autoinst.xml",
+          AutoinstConfig.profile_path,
           destdir,
           AutoinstConfig.xml_file
         )

test/AutoInstall_test.rb
@@ -106,8 +106,10 @@ describe "Yast::AutoInstall" do
     let(:scripts_dir) { "/scripts" }
 
     before do
+      allow(Yast::AutoinstConfig).to receive(:profile_path)
+        .and_return(FIXTURES_PATH.join("instsys", "tmp", "profile", "autoinst.xml").to_s)
       allow(Yast::AutoinstConfig).to receive(:tmpDir)
-        .and_return(FIXTURES_PATH.join("instsys", "tmp").to_s)
+        .and_return(FIXTURES_PATH.join("instsys", "tmp", "YaST2").to_s)
       allow(Yast::AutoinstConfig).to receive(:logs_dir).and_return(logs_dir)
       allow(Yast::AutoinstConfig).to receive(:scripts_dir).and_return(scripts_dir)
       FileUtils.mkdir(File.join(destdir, logs_dir))