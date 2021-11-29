715324d415b7d8ce2d82cdd73ac4b18fa9f818e1
Imobach González Sosa <igonzalezsosa@suse.com>
2021-11-29 10:17:57 +0000

    Extend AutoinstSoftware unit tests

--- test/AutoinstSoftware_test.rb
+++ test/AutoinstSoftware_test.rb
@@ -13,6 +13,8 @@ describe Yast::AutoinstSoftware do
   let(:profile) { FIXTURES_PATH.join("profiles", "software.xml").to_s }
 
   before(:each) do
+    Yast::AutoinstData.main
+    subject.main
     Yast::Profile.ReadXML(profile)
   end
 
@@ -53,6 +55,43 @@ describe Yast::AutoinstSoftware do
     end
   end
 
+  describe "#Import" do
+    let(:software) do
+      {
+        "patterns"        => ["base", "yast2_basis"],
+        "packages"        => ["yast2", "other"],
+        "post-patterns"   => ["gnome"],
+        "post-packages"   => ["pkg1"],
+        "remove-packages" => ["dummy"],
+        "kernel"          => "kernel-vanilla"
+      }
+    end
+
+    let(:available_pkgs) { ["yast2"] }
+
+    before do
+      allow(Yast::Pkg).to receive(:GetPackages).with(:available, true).and_return(available_pkgs)
+    end
+
+    it "saves the list of patterns and packages to install and remove" do
+      subject.Import(software)
+      expect(subject.patterns).to eq(["base", "yast2_basis"])
+      expect(Yast::PackageAI.toinstall).to eq(["yast2", "other"])
+      expect(Yast::PackageAI.toremove).to eq(["dummy"])
+    end
+
+    it "sets the kernel package to install" do
+      subject.Import(software)
+      expect(subject.kernel).to eq("kernel-vanilla")
+    end
+
+    it "saves the list of patterns and packages to install during 2nd stage" do
+      subject.Import(software)
+      expect(Yast::AutoinstData.post_patterns).to eq(["gnome"])
+      expect(Yast::AutoinstData.post_packages).to eq(["pkg1"])
+    end
+  end
+
   describe "#Export" do
     it "puts product definition into the exported profile" do
       expect(Yast::Product)
@@ -88,6 +127,7 @@ describe Yast::AutoinstSoftware do
     end
 
     it "shows a popup if some packages have not been found" do
+      subject.Import(Yast::Profile.current["software"])
       expect(Yast::Pkg).to receive(:DoProvide).with(["a2", "a3", "a4"]).and_return({})
       expect(Yast::Report).to receive(:Error)
       subject.SelectPackagesForInstallation()
@@ -137,4 +177,103 @@ describe Yast::AutoinstSoftware do
     end
   end
 
+  describe "#Write" do
+    let(:base_product) { { "name" => "Leap" } }
+    let(:selected_product) do
+      instance_double(Y2Packager::Product, select: nil)
+    end
+    let(:storage_manager) do
+      instance_double(Y2Storage::StorageManager, staging: devicegraph)
+    end
+
+    let(:devicegraph) do
+      instance_double(Y2Storage::Devicegraph, used_features: storage_features)
+    end
+
+    let(:storage_features) do
+      instance_double(Y2Storage::StorageFeaturesList, pkg_list: storage_pkgs)
+    end
+
+    let(:storage_pkgs) { ["btrfsprogs"] }
+    let(:solver_result) { true }
+
+    let(:storage_pkg_handler) do
+      instance_double(Y2Storage::PackageHandler, set_proposal_packages: true)
+    end
+
+    let(:pkgs_to_install) { ["openSUSE-release"] }
+
+    before do
+      allow(Yast::Packages).to receive(:Init)
+      allow(Yast::Product).to receive(:FindBaseProducts).and_return([base_product])
+      allow(Yast::Pkg).to receive(:PkgApplReset)
+      allow(Yast::Pkg).to receive(:PkgSolve).and_return(solver_result)
+      allow(Y2Storage::StorageManager).to receive(:instance).and_return(storage_manager)
+      allow(Y2Storage::PackageHandler).to receive(:new).with(storage_pkgs)
+        .and_return(storage_pkg_handler)
+      allow(Yast::SpaceCalculation).to receive(:ShowPartitionWarning)
+      allow(Yast::AutoinstFunctions).to receive(:selected_product)
+        .and_return(selected_product)
+      allow(subject).to receive(:SelectPackagesForInstallation)
+      allow(Yast::Packages).to receive(:ComputeSystemPackageList).and_return(pkgs_to_install)
+      allow(Yast::Packages).to receive(:SelectSystemPatterns)
+      subject.kernel = nil
+    end
+
+    it "selects packages for installation" do
+      expect(Yast::Packages).to receive(:ComputeSystemPackageList)
+      expect(subject).to receive(:SelectPackagesForInstallation)
+      subject.Write
+    end
+
+    it "selects system patterns" do
+      expect(Yast::Packages).to receive(:SelectSystemPatterns).with(false)
+      subject.Write
+    end
+
+    it "sets base products for installation" do
+      expect(Yast::Pkg).to receive(:ResolvableInstall).with("Leap", :product)
+      expect(selected_product).to receive(:select)
+      subject.Write
+    end
+
+    it "selects required storage packages for installation" do
+      expect(storage_pkg_handler).to receive(:set_proposal_packages)
+      subject.Write
+    end
+
+    context "when a kernel package is selected" do
+      let(:pkgs_to_install) { ["kernel-default"] }
+
+      before do
+        subject.kernel = "kernel-vanilla"
+      end
+
+      it "removes other kernel packages" do
+        expect(Yast::Pkg).to receive(:PkgTaboo).with("kernel-default")
+        subject.Write
+      end
+    end
+
+    context "when packages are preselected for removal" do
+      before do
+        allow(Yast::PackageAI).to receive(:toremove).and_return(["dummy"])
+      end
+
+      it "removes the packages" do
+        expect(Yast::Pkg).to receive(:PkgTaboo).with("dummy")
+        expect(Yast::Pkg).to receive(:DoRemove).with(["dummy"])
+        subject.Write
+      end
+    end
+
+    context "when the resolver fails" do
+      let(:solver_result) { false }
+
+      it "displays an error" do
+        expect(Yast::Report).to receive(:LongError).with(/package resolver run failed/)
+        subject.Write
+      end
+    end
+  end
 end
